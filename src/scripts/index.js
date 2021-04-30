7a2728e82383db3fc23f40590dc0ea23cc4b7d33
Luis Gerardo <bis980428@gmail.com>
2021-04-30 18:19:05 -0500

    PrimerCommit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
 import '../styles/index.scss';
+import { Ejercicio } from './Ejercicio1';
 
-console.log('webpack starterkit');
+let Ejercicio1 = new Ejercicio();
+
+console.log(Ejercicio1.BusquedaCorridas('19 - AUTOBUSES INTERESTATALES DE  MEXICO, S.A. DE C.V.','AIM'));
